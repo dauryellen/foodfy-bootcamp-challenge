fbdefa6c02d52b9170926e805054340ab33282bb
Daury Mendes <dauryellen@live.com>
2020-07-23 21:08:06 -0300

    modal finalizado

--- scripts.js
+++ scripts.js
@@ -4,7 +4,16 @@ const recipes = document.querySelectorAll(".card");
 
 for (let recipe of recipes) {
   recipe.addEventListener("click", () => {
+    const image = recipe.querySelector("img").getAttribute("src");
+    const alt = recipe.querySelector("img").getAttribute("alt");
+    const h3Title = recipe.querySelector("h3").innerText;
+    const pAuthor = recipe.querySelector("p").innerText;
+
     modalOverlay.classList.add("active");
+    modalOverlay.querySelector("img").src = image;
+    modalOverlay.querySelector("img").alt = alt;
+    modalOverlay.querySelector("h3").innerHTML = h3Title;
+    modalOverlay.querySelector("p").innerHTML = pAuthor;
   });
 }
 
