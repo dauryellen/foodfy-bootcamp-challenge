b9597bc4fd52aae785ee791f175342a583fc78ac
Daury Mendes <dauryellen@live.com>
2020-07-27 19:25:37 -0300

    configuração do nunjucks como view engine

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const server = express();
 
 server.use(express.static("public"));
 
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+  express: server,
+  noCache: true,
+  autoescape: false,
+});
+
 server.listen(5000, () => {
   console.log("Server is running.");
 });
